5dce27f455e9bef9f7886f3c8a7dc908ad5bdada
amnasb <amnasb03@gmail.com>
2026-04-29 01:26:00 +0500

    jsonify remove

--- app/agents/coordinator_agent.py
+++ app/agents/coordinator_agent.py
@@ -1,3 +1,5 @@
+from http.client import responses
+
 from app.agents.dashboard_agent import DashboardAgent
 from app.agents.risk_agent import RiskAgent
 from app.agents.planning_agent import PlanningAgent
@@ -21,24 +23,24 @@ class CoordinatorAgent:
     def seasonal_planning(self, user_id):
         seasonal_planner_status = self.planning_agent.generate_seasonal_plan(user_id, tag="seasonal_planning ")
         if seasonal_planner_status == enums_.Status.SUCCESS:
-            return jsonify({"message": "Seasonal planning completed"}), 200
+            return responses.success("Seasonal planning completed")
         else:
-            return jsonify({"message": "Seasonal planning failed"}), 500
+            return responses.failure("Seasonal planning failed")
         
     def weekly_planning(self, user_id):
         weekly_planner_status = self.planning_agent.generate_weekly_plan(user_id, tag="weekly_planning ")
         if weekly_planner_status == enums_.Status.SUCCESS:
-            return jsonify({"message": "Weekly planning completed"}), 200
+            return responses.success("Weekly planning completed")
         else:
-            return jsonify({"message": "Weekly planning failed"}), 500
+            return responses.failure("Weekly planning failed")
         
     def task_generation(self, user_id):
         daily_planner_status = self.planning_agent.generate_daily_tasks(user_id, tag="daily_planning ")
         self.dashboard_agent.refresh_dashboard(user_id)
         if daily_planner_status == enums_.Status.SUCCESS:
-            return jsonify({"message": "Daily planning completed"}), 200
+            return responses.success("Daily planning completed")
         else:
-            return jsonify({"message": "Daily planning failed"}), 500
+            return responses.failure("Daily planning failed")
 
     def daily_update(self, user_id):
         risk_assessment_status, change = self.risk_agent.assess_risk(user_id, tag="risk_assessment ")
@@ -65,30 +67,30 @@ class CoordinatorAgent:
         self.dashboard_agent.refresh_dashboard(user_id)
         
         if risk_assessment_status == enums_.Status.SUCCESS:
-            return jsonify({"message": "Risk assessment completed"}), 200
+            return responses.success("Risk assesment completed")
         else:
-            return jsonify({"message": "Risk assessment failed"}), 500
+            return responses.failure("Risk assessment failed")
 
     def dashboard_refresh(self, user_id):
         dashboard_refresh_status =  self.dashboard_agent.refresh_dashboard(user_id)
         if dashboard_refresh_status == enums_.Status.SUCCESS:
-            return jsonify({"message": "Risk assessment completed"}), 200
+            return responses.success("Dashboard update completed")
         else:
-            return jsonify({"message": "Risk assessment failed"}), 500
+            return responses.failure("Dashboard update failed")
 
     def call_advisor(self, user_id):
         advisor_call_status = self.advisory_agent.generate_advisory(user_id, tag="advisory_generation ")
         if advisor_call_status == enums_.Status.SUCCESS:
-            return jsonify({"message": "Advisory generation completed"}), 200
+            return responses.success("Advisor call completed")
         else:
-            return jsonify({"message": "Advisory generation failed"}), 500
+            return responses.failure("Advisor call failed")
 
     def send_alert(self, user_id, tag):
         alert_status = self.alert_agent.generate_alerts(user_id, tag=tag)
         if alert_status == enums_.Status.SUCCESS:
-            return jsonify({"message": "Alert generation completed"}), 200
+            return responses.success("Alert update completed")
         else:
-            return jsonify({"message": "Alert generation failed"}), 500
+            return responses.failure("Alert update failed")
 
 
         
